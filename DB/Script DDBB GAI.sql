82f98f71b08f43d72a241ba4b198321726ca01a4
Jordi Osarenkhoe Petro <95173613+Jordiop@users.noreply.github.com>
2022-06-05 23:44:15 +0200

    Update Script DDBB GAI.sql

--- DB/Script DDBB GAI.sql
+++ DB/Script DDBB GAI.sql
@@ -186,4 +186,4 @@ INSERT INTO Almacen VALUES (1,"Primero","Caracas 06");
 INSERT INTO Treballadors VALUES (1,1,"GOD","0000","God","OST","DIOS");
 INSERT INTO Pasillos VALUES (1,1,1);
 INSERT INTO Estanterias VALUES (1,1,1,10,10);
-INSERT INTO Productos (1,1,1,"Gambas",500,"mar", 5,5);
+INSERT INTO Productos VALUES (1,1,1,"Gambas",500,"mar", 5,5);
